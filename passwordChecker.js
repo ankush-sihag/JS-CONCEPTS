646c376999ea258e6ee0ca74c7bde29abcb61cba
ankush-sihag <ankushsihag7@gmail.com>
2026-06-25 16:13:40 +0530

    password checker

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,20 +1,39 @@
-const password = "Hello123";
+// const password = "Hello123";
 
-const hasUpperCase = /[A-Z]/.test(password);
+// const hasUpperCase = /[A-Z]/.test(password);
 
-const hasLowerCase = /[a-z]/.test(password);
+// const hasLowerCase = /[a-z]/.test(password);
+
+// const hasNumber = /[0-9]/.test(password);
+
+// if (
+//   password.length >= 8 &&
+//   hasUpperCase &&
+//   hasLowerCase &&
+//   hasNumber
+// ) {
+//   console.log("Strong Password");
+// } else {
+//   console.log("Weak Password");
+// }
+
+// console.log(password.length);
+
+const password = "AnkushSihag28";
+
+const hasUppercase = /[A-Z]/.test(password);
+
+const hasLowercase = /[a-z]/.test(password);
 
 const hasNumber = /[0-9]/.test(password);
 
 if (
-  password.length >= 8 &&
-  hasUpperCase &&
-  hasLowerCase &&
+  password.length>=9 &&
+  hasUppercase&&
+  hasLowercase&&
   hasNumber
 ) {
-  console.log("Strong Password");
+  console.log('Good Password')
 } else {
-  console.log("Weak Password");
-}
-
-console.log(password.length);
+  console.log('Week Password')
+};
